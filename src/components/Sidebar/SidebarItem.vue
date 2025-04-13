1e860642025a88a93707d243ccc6e06edbb55029
AdonisinodA <adonisinoda07@gmail.com>
2025-04-13 08:21:00 -0300

    feat: cria componente item em sidebar

---
+++ src/components/Sidebar/SidebarItem.vue
@@ -0,0 +1,20 @@
+<template>
+    <li
+      :class="[
+        'flex items-center space-x-2 p-2 rounded hover:bg-gray-100 cursor-pointer',
+        active ? 'bg-green-50 text-green-600' : ''
+      ]"
+    >
+      <span>{{ icon }}</span>
+      <span>{{ text }}</span>
+    </li>
+  </template>
+  
+  <script setup lang="ts">
+  defineProps<{
+    icon: string
+    text: string
+    active?: boolean
+  }>()
+  </script>
+  
