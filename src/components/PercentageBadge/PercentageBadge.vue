94fee5b11951e0cee797a8f9aaeb8de43c8cf2bc
AdonisinodA <adonisinoda07@gmail.com>
2025-04-13 11:22:22 -0300

    feat: cria componente de porcentagem

---
+++ src/components/PercentageBadge/PercentageBadge.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="inline-flex items-center px-2 py-1 rounded bg-green-100 text-green-600 text-sm font-medium">
+      <component :is="icon" class="w-4 h-4 mr-1" />
+      <span>{{ percentage }}%</span>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import { defineProps } from 'vue'
+  
+  const { icon, percentage} = defineProps<{
+    percentage: number
+    icon: any
+  }>()
+  </script>
+  
---
+++ src/components/PercentageBadge/PercentageUp.vue
@@ -0,0 +1,17 @@
+<template>
+    <PercentageBadge :percentage='value' :icon="CoChartLine"></PercentageBadge>
+</template>
+
+
+
+<script setup lang="ts">
+  import { defineProps } from 'vue'
+import PercentageBadge from './PercentageBadge.vue';
+import { CoChartLine } from '@kalimahapps/vue-icons';
+
+
+const {value} = defineProps<{
+    value:number 
+}>()
+
+</script>
