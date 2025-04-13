34a7b4fd9fd1ce86301508978e2255ca229e21ac
AdonisinodA <adonisinoda07@gmail.com>
2025-04-13 08:18:26 -0300

    chore: configura projeto

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Sidebar from './components/Sidebar/Sidebar.vue'
+import Dashboard from './components/Dashboard/Dashboard.vue'
+</script>
+
+<template>
+  <div class="flex min-h-screen bg-gray-100">
+    <Sidebar />
+    <Dashboard />
+  </div>
+</template>
