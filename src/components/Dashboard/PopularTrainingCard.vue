191a784d436a8498ce049b31fff0b931272a94a4
AdonisinodA <adonisinoda07@gmail.com>
2025-04-13 08:20:22 -0300

    cria componente de treinamentos em dashboard

---
+++ src/components/Dashboard/PopularTrainingCard.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="p-4 border rounded-lg text-center">
+      <h3 class="font-semibold">{{ name }}</h3>
+      <p class="text-sm text-gray-600">{{ users }} Alunos</p>
+      <p class="text-sm text-gray-600">{{ duration }}</p>
+      <p class="text-sm text-green-500">{{ enrolled }} matriculados</p>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  defineProps<{
+    name: string
+    users: string
+    duration: string
+    enrolled: string
+  }>()
+  </script>
+  
