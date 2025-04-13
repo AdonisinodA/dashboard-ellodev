1830c9b5908198b91d58233b1c79dc0e00b492fa
AdonisinodA <adonisinoda07@gmail.com>
2025-04-13 08:19:35 -0300

    feat: cria statCard componente

---
+++ src/components/Dashboard/StatCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="bg-white p-6 rounded-lg shadow">
+      <div class="flex justify-between items-center">
+        <div>
+          <p class="text-gray-600">{{ label }}</p>
+          <p class="text-2xl font-bold">{{ value }}</p>
+        </div>
+        <span class="text-green-500">+{{ increase }}</span>
+      </div>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  defineProps<{
+    label: string
+    value: string
+    increase: string
+  }>()
+  </script>
+  
